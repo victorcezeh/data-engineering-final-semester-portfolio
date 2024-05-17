a0ca1fe716a45b83c4fddbc10bfca77dccca7794
Your Name <your_email@example.com>
2024-05-17 06:48:32 +0100

    Setup Infrastructure

--- postgres_docker_init/infrastructure_scripts/init.sql
+++ postgres_docker_init/infrastructure_scripts/init.sql
@@ -1,23 +1,23 @@
 -- create schema
-CREATE SCHEME IF NOT EXISTS INK_STORE;
+CREATE SCHEMA IF NOT EXISTS INK_STORE;
 
 -- create and populate tables
-create table if not exists INK_STORES.CUSTOMERS
+create table if not exists INK_STORE.CUSTOMERS
 (
-    customer_key int primary key,
-    prefix varchar(3),
+    customer_key serial primary key,
+    prefix varchar(10),
     first_name varchar(255),
     last_name varchar(255),
-    birth_month date,
-    marital_status char(1),
-    gender char(1),
+    birth_month varchar(25),
+    marital_status varchar(10),
+    gender varchar(10),
     email_address varchar(255),
-    annual_income int,
+    annual_income varchar(255),
     total_children int,
     educational_level varchar(50),
     occupation varchar(255),
-    home_owner char(1)
+    home_owner varchar(10)
 );
 
-COPY INK_STORE.CUSTOMERS ("customer key", prefix, "first name", "last name", "birth month", "marital status", gender, "email address", "annual income", "total children", "educational level", occupation, "home owner")
+COPY INK_STORE.CUSTOMERS (customer_key, prefix, first_name, last_name, birth_month, marital_status, gender, email_address, annual_income, total_children, educational_level, occupation, home_owner)
 FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
